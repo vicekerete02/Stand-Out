045fb56531ce8386b55635c644b0c3d94c7f33e4
vicekerete02 <vicekerete02@gmail.com>
2019-07-10 15:57:14 -0700

    Stand out project

---
+++ Stand Out/script.js
@@ -0,0 +1,7 @@
+var person = prompt("Please enter your name",);
+if (person != null) {
+    document.getElementById("web").innerHTML =
+    "Hello " + person + "! How are you today?";
+}
+
+   document.getelementsById()
